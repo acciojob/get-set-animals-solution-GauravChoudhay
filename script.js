3c9a93c4c98d2be391f665d2d3043c3a173a4956
GAURAV CHOUDHARY <46090043+GauravChoudhay@users.noreply.github.com>
2023-04-08 21:52:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 class Animal {
-  constructor(species) {
-    this._species = species;
-  }
+constructor(species) {
+this._species = species;
+}
 
-  get species() {
-    return this._species;
-  }
+get species() {
+return this._species;
+}
 
-  makeSound() {
-    console.log("The " + this.species + " makes a sound");
-  }
+makeSound() {
+console.log("The " + this.species + " makes a sound");
+}
 }
 
 class Cat extends Animal {
-  purr() {
-    console.log("purr");
-  }
+constructor(species) {
+super(species);
+}
+purr() {
+console.log("purr");
+}
 }
 
 class Dog extends Animal {
-  bark() {
-    console.log("woof");
-  }
+constructor(species) {
+super(species);
 }
+bark() {
+console.log("woof");
+}
+}
+let animal = new Animal("Horse");
+animal.makeSound();
+let cat = new Cat("Cat");
+cat.purr();
+let dog = new Dog("Dog");
+dog.bark();
 
 // Do not change the code below this line
 window.Animal = Animal;
